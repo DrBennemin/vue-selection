eeb24e5baf530a25b95b01358b01d048d2853222
Benjamin Krzis <benjamin@elisana.digital>
2020-10-25 14:22:18 +0100

    Add function to hide selected users

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -1,20 +1,42 @@
 <template>
   <div>
-    <span>
-      Selected Items x
-    </span>
-    <span>
-      Selected Items x
-    </span>
-    <span>
-      Selected Items x
-    </span>
+    <div>
+      <ul>
+        <li v-for="user in selectedUsers" :key="user">
+          {{ user }}
+        </li>
+      </ul>
+    </div>
+    <ul>
+      <li @click="addUserToList(user)" v-for="(user, key) in users" :key="key">
+        {{ user }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Selection",
+  user: "Listitems",
+  data: function() {
+    return {
+      users: [
+        "Boris Becker",
+        "Steffi Graf",
+        "Karl Dall",
+        "Hennes Bender",
+        "Rudi Karell",
+        "Axel Stein",
+        "Roberto Blanko",
+      ],
+      selectedUsers: [],
+    };
+  },
+  methods: {
+    addUserToList(user) {
+      this.selectedUsers.push(user);
+    },
+  },
 };
 </script>
 
